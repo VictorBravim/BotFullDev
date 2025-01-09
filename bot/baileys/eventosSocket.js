ba9ccb33c0f83fe605a73a934a695c3b36f8a541
Victor Bravim <victorrafaelbravim@gmail.com>
2025-01-09 10:05:06 -0300

    Add

--- bot/baileys/eventosSocket.js
+++ bot/baileys/eventosSocket.js
@@ -156,5 +156,3 @@ export const atualizacaoDadosGrupo = async (dadosGrupo)=>{
         consoleErro(err, "GROUPS.UPDATE")
     }
 }
-
-
--- bot/lib/util.js
+++ bot/lib/util.js
@@ -130,4 +130,3 @@ export async function criarArquivosNecessarios(){
           throw new Error(err)
       }
 }
-
